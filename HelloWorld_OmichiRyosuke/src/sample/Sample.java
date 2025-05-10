ae99cf3a8c2d7f15631ec451033f28e7b6bee2ed
oomt2 <oomt2@192.168.0.10>
2025-05-10 23:27:49 +0900

    a

--- HelloWorld_OmichiRyosuke/src/sample/Sample.java
+++ HelloWorld_OmichiRyosuke/src/sample/Sample.java
@@ -3,7 +3,7 @@ package sample;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,World");
+		System.out.println("Hello,World!");
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
