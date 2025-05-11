f9c96ce66b0ddcbed5ed32ccb36e6f736dc5bcbc
oomt2 <oomt2@192.168.0.10>
2025-05-12 02:15:20 +0900

    a

---
+++ HelloWorld_OmichiRyosuke/src/sample/SelfIntroduction.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[]args) {
+		String name ="大道　亮祐";
+		int age =  25;
+		String hobby  ="映画鑑賞";
+		
+		System.out.println("私の名前は"+ name +"です。");
+		System.out.println("年齢は"+ age+"歳です。");
+		System.out.println("趣味は"+ hobby +"です。");
+				
+		
+	}
+
+
+
+}
