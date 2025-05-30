8f1a25c7364c6e16f1fb5865e0db64628ac8cd14
oomt2 <oomt2@192.168.0.10>
2025-05-30 19:23:27 +0900

    課題8おわりました

---
+++ HelloWorld_OmichiRyosuke/src/curriculum_A/Qes1.java
@@ -0,0 +1,202 @@
+package curriculum_A;
+
+public class Qes1 {
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		byte byteNum;
+		short shortNum;
+		int intNum;
+		long lomgNum;
+		float floatNum;
+		double doubleNmu;
+		char letterNum;
+		String letter;
+		String letters;
+		boolean isBoolean;
+	
+		
+		
+
+		byteNum = 10;
+		shortNum = 100;
+		intNum = 1000;
+		lomgNum = 10000;
+		floatNum = 9.5f;
+		doubleNmu = 10.5;
+		letter = "a";
+		letters = "ハロー";
+		isBoolean = true;
+		
+		
+		
+		
+		
+		class main {
+		    public static void main(String[] args) {
+		        
+		        
+		        System.out.println(byteNum+shortNum+intNum+lomgNum);
+		    }
+		}
+		
+		 class main1 {
+		    public static void main(String[] args) {
+		    	System.out.println(byteNum+byteNum);  
+		}
+		 }
+		
+		 class main {
+			    public static void main(String[] args) {
+			        
+			        
+			        System.out.println(letter+letters+isBoolean);
+			    }
+		
+			    
+			    
+			    
+			    
+			    class main {
+				    public static void main(String[] args) {
+				        
+				        System.out.println(byteNum+shortNum+intNum+lomgNum+floatNum+doubleNmu);
+				    }
+		
+				    class main {
+					    public static void main(String[] args) {
+					    	
+					    
+					    	System.out.println(byteNum*shortNum*intNum*lomgNum);  
+					    	{
+					    		
+					    	}
+					    	
+					    	 class main {
+					 		    public static void main(String[] args) {
+					 		    	    	System.out.println(doubleNmu/shortNum);  
+					 				    	
+					    	
+					    	
+					 		class main {
+							    public static void main(String[] args) {
+							    	System.out.println(byteNum-shortNum);  
+							}
+							 }			    	
+					    	
+					    	
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        String name = "山田太郎";
+					 		        System.out.println("こんにちは、" + name + "さん！");
+					 		    }
+					 		}		    	
+					    	
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int age = 25;
+					 		        System.out.println("年齢: " + age + "歳");
+					 		    }
+					 		}
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int num1 = 10;
+					 		        int num2 = 5;
+					 		        int sum = num1 + num2;
+
+					 		        System.out.println("合計: " + sum);
+					 		    }
+					 		}
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int score = 80;
+					 		        score = score + 20;
+
+					 		        System.out.println("最終スコア: " + score);
+					 		    }
+					 		}
+
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        double price = 99.99;
+					 		        int intPrice = (int) price;
+
+					 		        System.out.println("整数価格: " + intPrice);
+					 		    }
+					 		}
+ 	
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        String numStr = "123"; 
+					 		        int num = Integer.parseInt(numStr);
+					 		        int result = num + 10;
+
+					 		        System.out.println("変換後の値: " + result);
+					 		    }
+					 		}
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int num = 50;
+					 		        String numStr = String.valueOf(num);
+
+					 		        System.out.println("得点: " + numStr + "点");
+					 		    }
+					 		}
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int a = 10;
+					 		        System.out.println("aの値は: " + a);
+					 		    }
+					 		}
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int b = 20;
+					 		        System.out.println("bの値は: " + b);
+					 		    }
+					 		}
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int a = 10;
+					 		        int b = 20;
+					 		        boolean result = (a < b);
+
+					 		        System.out.println("aがbより小さいか: " + result);
+					 		    }
+					 		}
+
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int a = 10;
+					 		        int b = 20;
+					 		        boolean result = (a < b);
+
+					 		        System.out.println(result);
+					 		    }
+					 		}
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int x = 15;
+					 		        System.out.println("xの値は: " + x);
+					 		    }
+					 		}
+					
+					 		public class Main {
+					 		    public static void main(String[] args) {
+					 		        int x = 15;
+					 		        String result = (x >= 10) ? "OK" : "NG";
+
+					 		        System.out.println(result);
+				 		
+					 		  
+					 		    	    }
+					 		    	}
+					 		      public class Main {
+					 		    	    public static void main(String[] args) {
+					 		    	        String text = "私はJavaが好きです。Javaは楽しい！";
+					 		    	        String replacedText = text.replace("Java", "Python");
+
+					 		    	        System.out.println(replacedText);
+					 		    	    }
+					 		    	}
+		
+					 	
